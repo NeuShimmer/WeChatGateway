5011565da5076754ea498f08c29920cb2ced6baa
ShuangYa <896640547@qq.com>
2018-08-29 17:00:06 +0800

    Add 数据库和安装说明

--- README.md
+++ README.md
@@ -6,3 +6,19 @@
 微信的一些API封装
 
 接口文档使用[APIDoc](http://apidocjs.com/)生成
+
+### 安装说明
+
+* 环境需求：PHP 7.0+，Swoole 4.0+，Swoole Serialize，Redis，MySQL，Composer
+
+* 下载源代码并解压
+
+* 进入源代码目录，执行`composer install`
+
+* 将`database.sql`中的内容导入MySQL
+
+* 修改`application/Config.ini`中相应配置（包括静态文件地址、MySQL连接、Redis连接、Swoole相关配置等）
+
+* 修改`application/Bootstrap.php`中的`Yesf::setBaseUri('/wechat/');`部分为您实际部署的路径
+
+* 进入后台（`/index/admin/index`），输入默认密码`admin`，进行进一步配置
---
+++ database.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 2018-08-29 16:53:19
+-- 服务器版本： 5.7.20-log
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wechat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `app`
+--
+
+CREATE TABLE `app` (
+  `id` smallint(6) UNSIGNED NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `appid` varchar(50) NOT NULL,
+  `appsecret` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `config`
+--
+
+CREATE TABLE `config` (
+  `id` varchar(20) NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 转存表中的数据 `config`
+--
+
+INSERT INTO `config` (`id`, `value`) VALUES
+('admin_password', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
+('appid', ''),
+('appsecret', ''),
+('cookie_domain', '.example.com'),
+('redirect_uri', '');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(10) UNSIGNED NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `openid` varchar(50) NOT NULL,
+  `unionid` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `app`
+--
+ALTER TABLE `app`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `openid` (`openid`),
+  ADD UNIQUE KEY `unionid` (`unionid`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `app`
+--
+ALTER TABLE `app`
+  MODIFY `id` smallint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
