7ca4cd462a40de0b0609a6cb1f70e5b7c9815996
ShuangYa <896640547@qq.com>
2018-09-10 23:21:09 +0800

    Update Yesf version

--- start.php
+++ start.php
@@ -4,5 +4,5 @@ define("APP_PATH",  __DIR__ . '/application/');
 //使用Composer
 require(__DIR__ . '/vendor/autoload.php');
 //初始化
-$app = new yesf\Yesf(APP_PATH . "/Config.ini");
-$app->bootstrap()->run();
+$app = new yesf\Yesf();
+$app->bootstrap()->run(APP_PATH . "/config/env.ini");
